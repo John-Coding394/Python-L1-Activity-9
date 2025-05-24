6b51722f76fde6b3e69df7b0946cdd00761ad677
John-Coding394 <johnchaghouri394@gmail.com>
2025-05-24 10:30:06 -0400

    First commit

---
+++ Activity1.py
@@ -0,0 +1,7 @@
+num = int(input("Put in a number"))
+sum = 0
+for i in range (1,num+1):
+    sum=sum+i
+    print("Value of i ",i)
+    print("Value of sum ",sum)
+print("Sum of n whole numbers are ",sum)
---
+++ main.py
@@ -0,0 +1,3 @@
+num = int(input("Insert a number"))
+for i in range (num):
+    print("Hello", i)
