bac71dc2c4a506e0ae6fdfa6dceefc0121eddb27
John-Coding394 <johnchaghouri394@gmail.com>
2025-05-24 10:33:56 -0400

    Second commit

--- Activity2.py
+++ Activity2.py
@@ -2,6 +2,6 @@ string = input("Please input your own string")
 string2 = ('')
 for i in string:
     string2 = i + string2
-
+    print("The value of string2", string2)
 print("\nThe original string = ", string)
 print("The reversed string = ", string2)
